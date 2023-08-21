cc8032579f92d93e76aafd50179885ed90689f05
arleth cordero <arleth.cordero09@gmail.com>
2023-08-21 19:06:25 -0300

    perfilmatch con vue

--- perfilmatch/src/App.vue
+++ perfilmatch/src/App.vue
@@ -1,30 +1,47 @@
 <script setup>
-import HelloWorld from './components/HelloWorld.vue'
+//import HelloWorld from './components/HelloWorld.vue'
 </script>
 
 <template>
-  <div>
-    <a href="https://vitejs.dev" target="_blank">
-      <img src="/vite.svg" class="logo" alt="Vite logo" />
-    </a>
-    <a href="https://vuejs.org/" target="_blank">
-      <img src="./assets/vue.svg" class="logo vue" alt="Vue logo" />
-    </a>
-  </div>
-  <HelloWorld msg="Vite + Vue" />
-</template>
+ <div>
+   <h1>Animatch</h1>
 
-<style scoped>
-.logo {
-  height: 6em;
-  padding: 1.5em;
-  will-change: filter;
-  transition: filter 300ms;
-}
-.logo:hover {
-  filter: drop-shadow(0 0 2em #646cffaa);
-}
-.logo.vue:hover {
-  filter: drop-shadow(0 0 2em #42b883aa);
-}
-</style>
+    <div class="logo"><img  src="./assets/img/logoanimatch.png"></div>
+    <div class="nav">hola</div> 
+    <div class="swipe-container">
+      <div class="swipe-card">
+       <img src= "./assets/img/m.jpg">
+        <div class="card-overlay">
+          <p class="card-text">Lucas 8</p>
+         <p class="text">Lorem ipsum dolor sit amet consectetur, adipisicing elit. Molestiae sint nisi tenetur fuga similique deleniti cumque impedit ratione doloremque, consequatur libero quia reiciendis saepe modi odio asperiores. Cupiditate, ducimus velit?</p>
+       </div>
+     </div>
+     <div class="swipe-card">
+        <img src="./assets/img/edgar.jpg">
+       <div class="card-overlay">
+         <p class="card-text">Edgar 6</p>
+         <p class="text">Lorem ipsum dolor sit amet consectetur, adipisicing elit. Molestiae sint nisi tenetur fuga similique deleniti cumque impedit ratione doloremque, consequatur libero quia reiciendis saepe modi odio asperiores. Cupiditate, ducimus velit?</p>
+       </div>
+     </div>
+     <div class="swipe-card">
+       <img src="./assets/img/mariano.jpg">
+       <div class="card-overlay">
+         <p class="card-text">Marian 4</p>
+          <p class="text">Lorem ipsum dolor sit amet consectetur, adipisicing elit. Molestiae sint nisi tenetur fuga similique deleniti cumque impedit ratione doloremque, consequatur libero quia reiciendis saepe modi odio asperiores. Cupiditate, ducimus velit?</p>
+       </div> 
+     </div>
+     <div class="swipe-card">
+       <img src="./assets/img/cat.jpg">
+       <div class="card-overlay">
+         <p class="card-text">Lolo 2</p>
+         <p class="text">Lorem ipsum dolor sit amet consectetur, adipisicing elit. Molestiae sint nisi tenetur fuga similique deleniti cumque impedit ratione doloremque, consequatur libero quia reiciendis saepe modi odio asperiores. Cupiditate, ducimus velit?</p>
+       </div> 
+     </div>
+   </div>
+   <div id="coteinerbutton">
+     <button><img src="./assets/img/eliminar.png"></button>
+     <button><img src= "./assets/img/favoritos.png"></button>
+   </div>
+ </div>
+</template>
+ 
