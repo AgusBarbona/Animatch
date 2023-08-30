520b04a3aac16e3dd14fe739bffa21de87a220df
Daniela Fernandez Laura <dfernandezl@fi.uba.ar>
2023-08-30 18:22:46 -0300

    Subo los archivos de la seccion Chat - Desarrollada con HTML y CSS

---
+++ Chat with Vite/animatch-project/src/components/Chat/HeaderMsj.vue
@@ -0,0 +1,49 @@
+<template>
+    <header class="HeaderMsjUserView">
+            <h1>Chats</h1>
+            <img class="logSupport" v-bind:src="logoUrl" alt="Icono de ayuda">
+    </header>
+</template>
+
+<script>
+import IconHelp from "./img/Help_icon-icons.com_55891.png"
+
+export default{
+        name: 'HeaderMsj',
+        data(){
+            return {
+                logoUrl: IconHelp
+            };
+        },
+    };
+</script>
+
+<style scoped>
+
+.HeaderMsjUserView {
+    display: flex;
+    background: linear-gradient(0deg, #ff6c6c, #facec9);
+    height: 65px;
+    flex-direction: row;
+    justify-content: space-between;
+    align-items: center;
+    padding: 2px 38px;
+    border-bottom: 1px solid #ff5c6c;
+}
+
+.HeaderMsjUserView h1{
+    color: white;
+    font-size: 1.6rem;
+    font-weight: bolder;
+}
+
+.HeaderMsjUserView img{
+    width: 45px;
+    filter:opacity(0.7);
+}
+
+.logSupport{
+    display: none;
+}
+
+</style>
---
+++ Chat with Vite/animatch-project/src/components/Chat/MsjsUser.vue
@@ -0,0 +1,24 @@
+<template>
+    <div class="msjsUser">
+        <div class="msjUserImg">
+            <img src="./assets/img/ImgUser1.jpeg" alt="Imagen de usuario 1">
+        </div>
+
+        <div class="msjUserText">
+            <h2>Lolo Dog</h2>
+            <p>Lorem ipsum dolor, sit amet consectetur adipisicing elit.</p>
+        </div>
+
+        <div class="msjUserTextLogo">
+            <img src="./assets/img/logo_animatch.png" alt="logo">
+        </div>
+    </div>
+</template>
+
+<script>
+
+</script>
+
+<style>
+
+</style>
