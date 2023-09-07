1ef77db1483c687c7e1ddb3d507d1aa10488c44d
Ana Laura Zanardi <zanardi921@gmail.com>
2023-09-07 10:31:27 -0300

    ultimo

---
+++ index.js
@@ -0,0 +1,33 @@
+const express = require("express");
+const bodyParser = require("body-parser")
+const cors = require('cors');
+
+const path = __dirname + '/views/dist/';
+
+const app = express();
+
+app.use(express.static(path));
+
+
+var corsOptions = {
+  origin: "http://localhost:8081"
+};
+
+app.use(cors(corsOptions));
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({ extended: true }));
+
+app.get('/', function (req,res) {
+  res.sendFile(path + "index.html");
+});
+
+const PORT = process.env.PORT || 8080;
+
+app.get("/api", ( req, res) => {
+ res.json({ message: "Bienvenido al server"})
+})
+
+
+app.listen(PORT, () => {
+    console.log("server listening on http:\\localhost:${PORT}");
+})
--- server/ejemplo.js
+++
@@ -1,8 +0,0 @@
-const express = require('express');
-const router = express.Router();
-
-router.get('/api/ejemplo', (req, res) => {
-    res.json({mensaje: 'te habla tu backend'});
-});
-
-module.exports = router;
