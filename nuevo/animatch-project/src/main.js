2e24f6bd00f9e8632ae6f6c9d15c0986b4a2290c
Ana Laura Zanardi <zanardi921@gmail.com>
2023-09-07 10:22:45 -0300

    conexion-back-front-alfin

--- nuevo/animatch-project/src/main.js
+++ nuevo/animatch-project/src/main.js
@@ -1,34 +1,12 @@
 import { createApp } from 'vue';
 import App from './App.vue';
 import router from './routes.js';
-import axios from 'axios';
+//import axios from 'axios';
 
 const app = createApp(App);
 
-app.config.globalProperties.$axios = axios; // Agrega Axios como una propiedad global para que puedas usarlo en cualquier componente
+//app.config.globalProperties.$axios = axios; // Agrega Axios como una propiedad global para que puedas usarlo en cualquier componente
 
-app.mixin({//Los mixins son una forma de compartir opciones entre múltiples componentes en Vue.
-  methods: {
-    fetchDataFromBackend() {
-      axios.get('http://localhost:3000/ejemplo')
-        .then(response => {
-          console.log(response.data);
-        })
-        .catch(error => {
-          console.error(error);
-        });
-    }
-  }
-});
 
 app.use(router);
 app.mount('#app');
-
-/*import { createApp } from 'vue'
-import App from './App.vue'
-import router from './routes.js'; 
-
-const app = createApp(App);
-app.use(router); 
-app.mount('#app');
-createApp(App).use(router).mount('#app');*/
--- nuevo/animatch-project/src/routes.js
+++ nuevo/animatch-project/src/routes.js
@@ -6,7 +6,6 @@ import Matchs from '@/components/PerfilesMatch/perfilMatch.vue'
 import IniciarSesion from '@/Login/IniciarSesion.vue';
 import CrearCuenta from '@/Registro/CrearCuenta.vue';
 import Profile from '@/Profile/UserProfile.vue';
-import ComponenteEjemplo from '@/Back/ComponenteEjemplo.vue';
 
 const routes = [
     { path: '/', component: App },  
@@ -16,7 +15,6 @@ const routes = [
     { path: '/login', component: IniciarSesion },
     { path: '/registro', component: CrearCuenta },
     { path: '/micuenta', component: Profile},
-    { path: '/ejemplo', component: ComponenteEjemplo },
 ];
 
 const router = createRouter({
