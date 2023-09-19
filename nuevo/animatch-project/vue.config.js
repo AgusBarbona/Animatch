95bd4e023af0fe6640b8041be617b61e0d01193a
Ana Laura Zanardi <zanardi921@gmail.com>
2023-09-19 11:05:57 -0300

    doit

--- nuevo/animatch-project/vue.config.js
+++ nuevo/animatch-project/vue.config.js
@@ -1,13 +1,4 @@
 const { defineConfig } = require('@vue/cli-service')
 module.exports = defineConfig({
   transpileDependencies: true,
-  /*proxyTable: {
-    '/api':{
-        target: 'http://localhost:3000',
-        changeOrigin: true,
-        pathRewrite: {
-        '^/api': ''
-        }
-     }
-}*/
 })
